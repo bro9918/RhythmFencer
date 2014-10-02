d241f0053fb93a046bcba0263f0fa52763cff0ff
Sam Trapp <set9069@rit.edu>
2014-10-02 14:24:38 -0400

    broke it for a sec

---
+++ Assets/Scripts/BeatManager.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BeatManager : MonoBehaviour {
+
+	private static BeatManager instance;
+	public static BeatManager Instance
+	{
+		get
+		{
+			if (instance == null)
+			{
+				instance = GameObject.FindGameObjectWithTag("Globals").GetComponent<BeatManager>();
+			}
+			return instance;
+		}
+	}
+	public int bpm = 120;
+	public AudioSource beat;
+	private float elapsedSinceBeat;
+	public List<GameObject> beatMovers;
+
+	void FixedUpdate()
+	{
+		float beatPeriod = 60.0f / bpm;
+
+
+		if (rhythmFader != null)
+		{
+			Color fadeColor = rhythmFader.renderer.material.color;
+			fadeColor.a = elapsedSinceBeat / beatPeriod;
+			Debug.Log(fadeColor);
+			rhythmFader.renderer.material.color = fadeColor;
+		}
+		
+
+		if (elapsedSinceBeat >= beatPeriod)
+		{
+			elapsedSinceBeat = 0;
+			if (beat != null)
+			{
+				beat.Play();
+			}
+		}
+		else
+		{
+			elapsedSinceBeat += Time.fixedDeltaTime;
+		}
+	}
+}
