98a2e0748e7c4975178ad95065c45e9374df48e3
Bryan O'Neill <bro9918@g.rit.edu>
2014-10-02 20:47:51 -0400

    Added basic grid movement for each player, and some basic prefabs.

---
+++ Assets/Scripts/PlayerOneMovement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerOneMovement : MonoBehaviour {
+
+	public float xPosition = -3.0f;
+	private PlayerTwoMovement pTwoMove;
+	private GameObject pTwo;
+
+	// Use this for initialization
+	void Start () {
+		pTwo = GameObject.FindGameObjectWithTag("Player2");
+		pTwoMove = pTwo.GetComponent<PlayerTwoMovement>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(xPosition >-5.0f)
+		{
+			if(Input.GetKeyDown(KeyCode.A))
+			{
+				transform.Translate(new Vector3(-1.0f, 0.0f, 0.0f));
+				xPosition -= 1.0f;
+			}
+		}
+
+		if(xPosition < 1.0f && (pTwoMove.xPosition - xPosition) >= 2.0f)
+		{
+			if(Input.GetKeyDown(KeyCode.D))
+			{
+				transform.Translate(new Vector3(1.0f, 0.0f, 0.0f));
+				xPosition += 1.0f;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerTwoMovement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerTwoMovement : MonoBehaviour {
+
+	public float xPosition = 2.0f;
+	private PlayerOneMovement pOneMove;
+	private GameObject pOne;
+
+	// Use this for initialization
+	void Start () {
+		pOne = GameObject.FindGameObjectWithTag("Player1");
+		pOneMove = pOne.GetComponent<PlayerOneMovement>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(xPosition > -2.0f && (xPosition - pOneMove.xPosition) >= 2.0f)
+		{
+			if(Input.GetKeyDown(KeyCode.LeftArrow))
+			{
+				transform.Translate(new Vector3(-1.0f, 0.0f, 0.0f));
+				xPosition -= 1.0f;
+			}
+		}
+		
+		if(xPosition < 4.0f)
+		{
+			if(Input.GetKeyDown(KeyCode.RightArrow))
+			{
+				transform.Translate(new Vector3(1.0f, 0.0f, 0.0f));
+				xPosition += 1.0f;
+			}
+		}
+	}
+}
